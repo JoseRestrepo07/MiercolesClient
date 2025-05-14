c88ba98c35f0a18abf46fbbe0d1aadd3eb0c17b3
sebastian ramirez jaramillo <sramirezja@cesde.net>
2025-05-14 10:24:27 -0500

    avances

---
+++ src/components/pages/Analitica/Analitica.jsx
@@ -0,0 +1,25 @@
+export function Analitica() {
+    return (
+        <>
+        <section className="container">
+            <section className="row">
+                <section className="col-12 text-center">
+                    <h1>Analisis de datos</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium at maiores, non quibusdam eaque iusto corrupti nesciunt dicta. Iste iure velit quibusdam rem consequuntur libero quia eveniet praesentium. Voluptas, officia.</p>
+                    <hr />
+                </section>
+            </section>
+            <section className="row row-cols-1 row-cols-md-2"> 
+
+            </section>
+            <div className="col">
+                <div className="card my-3 shadow h-100 p-4">
+                    <h3>Asistencia vs Medio de transporte</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ad voluptatibus, impedit libero illo quia sapiente ex. Voluptates ea et repellat iure? Praesentium maxime quis sequi architecto doloremque reprehenderit odit.</p>
+                    <img src="../../../../src/assets/img/barras.webp" alt="barras" className="img-fluid"/>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
---
+++ src/components/pages/Api/Api.jsx
@@ -0,0 +1,23 @@
+export function Api (){
+    <>
+    <section className="container">
+        <section className="row">
+            <section className="col-12">
+                <h3>Control de asistencias: </h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit incidunt laborum vel veritatis minus cumque iste eos nisi ab soluta, quasi molestiae, reiciendis, cum quia itaque repellat nam nulla modi!</p>
+                <hr />
+            </section>
+        </section>
+        <section className="row">
+            <section className="col-12 col-md-8">
+                <form className="p-4 shadow my-3 border rounded">
+                    <input type="text" placeholder="estudiante" className="form-control mb-3"/>
+                    <input type="text" placeholder="curso" className="form-control mb-3"/>
+                    <input type="date" placeholder="fecha" className="form-control mb-3"/>
+                    <button className="btn btn-primary">Enviar</button>
+                </form>
+            </section>
+        </section>
+    </section>
+    </>
+}
